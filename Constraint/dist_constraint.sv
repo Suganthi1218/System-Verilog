b0c6f9f2e6efbb4629d600d0124db0cc059c9f26
Suganthi1218 <suganthi1218@gmail.com>
2024-10-25 21:13:10 +0530

    Create dist_constraint.sv

---
+++ Constraint/dist_constraint.sv
@@ -0,0 +1,26 @@
+class pack ;
+  
+  rand bit [3:0] a ;
+  
+  constraint co { a dist {1 :/2, [2:4]:/3 , 5:/4 };}
+  
+endclass
+
+
+module top;
+  
+  pack p ;
+  
+  initial begin 
+    
+    
+    p = new();
+    
+    for(int i= 1; i<9; i++) begin 
+      void'(p.randomize()) ;
+      
+      $display("%0t  iteration=%0d var=%0d ", $time, i, p.a);
+      
+    end
+  end
+endmodule
